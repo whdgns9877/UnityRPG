e51471b3bd0c25f5182774cc34a9b8e51646cb0c
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2023-07-26 20:26:41 +0900

    Change List<Transform> -> List<GameObject>

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -1,14 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Global : MonoBehaviour
 {
+    public List<GameObject> targets;
     // 인스턴스를 최초 한번만 생성하고 전역적으로 사용
     public static Global Inst { get; private set; }
-    private void Awake() => Inst = this;
+    private void Awake()
+    {
+        targets = new List<GameObject> ();
+        Inst = this;
+    }
 
-    public List<Transform> targets;
-    public void AddTarget(Transform target) => targets.Add(target);
-    public void RemoveTarget(Transform target) => targets.Remove(target);
+    public void AddTarget(GameObject target) => targets.Add(target);
+    public void RemoveTarget(GameObject target) => targets.Remove(target);
 }
